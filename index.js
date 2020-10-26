257eed45251375fd63affcb14fa35be4844569b9
bkk711 <boylaboy8@gmail.com>
2020-10-26 21:26:38 +0700

    edit token page

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ conn.connect(function(err) {
 });
 
 const bot = new BootBot({
-  accessToken: 'EABBYTTijjIABAA9R9FvZBiSin0Ht4eQzQmlCOWhHqZAJAwTGZC6mKZA14OjpWEqZB6WiYWNya3TSIzZAa3ZB9ICfwKzTyClyrjrdncMF6wh10w2StgqotzcztBpv6lYsz8ZBBgBgizSCDoYuk6pPJQCSZAgrO22pW3OZCO3l5pAr1npgZDZD',
+  accessToken: 'EABBYTTijjIABABQF87Iy6PnsYl8RDek4Pcz8M7tS6GJbWr807QUkcArEIwwLE8jrthDJwfDUuVszs9RZAnAkXYFriIf2Iey1MLcXFDMjT9qZCbSlxFQZCXVblSZChGXW8N56aS3lZBVwrm1Mb3ZC4VnrbEELZAXgV01i4k4EUXijQZDZD',
   verifyToken: 'covanhoctap',
   appSecret: '7b8270f3d08d81c366b7f8b5a6df64cc'
 });
@@ -34,6 +34,7 @@ bot.hear('hello', (payload, chat) => {
     chat.say(`Chào, ${user.first_name}!`);
   });
 });
+
 bot.on('message', (payload, chat) => {
   const text = payload.message.text;
   chat.getUserProfile().then((user) => {
