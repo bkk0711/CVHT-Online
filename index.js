6c16a1736ec32ba0d9bd2b6c1ac2c28c18b6d86c
bkk711 <boylaboy8@gmail.com>
2020-10-26 21:21:58 +0700

    add modules mysql, request and edit page fb

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict';
 const BootBot = require('bootbot');
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+  host    : 'localhost',
+  user    : 'root',
+  password: 'mysql',
+  database: 'cvht'
+});
+
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 const bot = new BootBot({
-  accessToken: 'EAALPml6OLosBALjTp1LO263ZBrBlSGAhPUm4xxdjNYDCw0NFJ475WmR2SpZAaFdmiGvprZAanKWPaaQrqqYmy89ZB0ZC9Mx3CTgpZBdbZA0AWkOdZBDypHZCm6OUhtHrE2hJXEnJuke8Ypd81BYH6s12MMT0dsflu7Kz2q1rAzxv55wZDZD',
+  accessToken: 'EABBYTTijjIABAA9R9FvZBiSin0Ht4eQzQmlCOWhHqZAJAwTGZC6mKZA14OjpWEqZB6WiYWNya3TSIzZAa3ZB9ICfwKzTyClyrjrdncMF6wh10w2StgqotzcztBpv6lYsz8ZBBgBgizSCDoYuk6pPJQCSZAgrO22pW3OZCO3l5pAr1npgZDZD',
   verifyToken: 'covanhoctap',
-  appSecret: '2d51e996225e93f05e83e9281a4fc029'
+  appSecret: '7b8270f3d08d81c366b7f8b5a6df64cc'
 });
 
 
@@ -23,6 +36,11 @@ bot.hear('hello', (payload, chat) => {
 });
 bot.on('message', (payload, chat) => {
   const text = payload.message.text;
-  chat.say(`Bạn nhắn: ${text}`);
+  chat.getUserProfile().then((user) => {
+  chat.say(`Bạn nhắn: ${text}`);  
+  console.log(`Người Gửi : ${user.first_name}`);
+  console.log(`Nội dung : ${text}`);
+  }); 
 });
+
 bot.start();
